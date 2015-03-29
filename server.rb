fb1281a13631e336edec2b262b2195cb4ca13e17
ablwr <ashley.blewer@gmail.com>
2015-03-29 17:04:21 -0400

    ditching public folder here as well

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ require 'json'
 
 puts 'Server started: http://localhost:3000/'
 
-root = File.expand_path './public'
+root = File.expand_path '.'
 server = WEBrick::HTTPServer.new :Port => 3000, :DocumentRoot => root
 
 trap 'INT' do server.shutdown end
